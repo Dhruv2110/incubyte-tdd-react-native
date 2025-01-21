9f81da4799b4c7fb10d43c1e7af36f5f039d6ec9
Dhruv Sharma <dhruvsharma.ynr@gmail.com>
2025-01-21 14:27:07 +0530

    init: initial commit

---
+++ App.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity } from 'react-native';
+
+const App = (): React.JSX.Element => {
+
+  const [input, setInput] = useState<string>('');
+  const [result, setResult] = useState<number>(0);
+
+  const onCalculate = () => {
+
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>String Calculator TDD</Text>
+      <TextInput
+        style={styles.textInput}
+        placeholder="Enter string of numbers"
+        value={input}
+        onChangeText={setInput}
+      />
+      <TouchableOpacity activeOpacity={0.7} onPress={onCalculate} style={styles.calculateBtn}>
+        <Text style={styles.calculateText}>
+          Calculate
+        </Text>
+      </TouchableOpacity>
+      {result !== null && <Text style={styles.sum}>Sum: {result}</Text>}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    justifyContent: 'center',
+    alignItems:'center'
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  textInput: {
+    width:'85%',
+    alignSelf:'center',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    borderRadius: 6,
+    marginBottom: 20,
+    fontSize: 16,
+    color:'black',
+  },
+  calculateBtn:{
+    width:'50%',
+    alignItems:'center',
+    justifyContent:'center',
+    paddingVertical:10,
+    borderRadius:6,
+    backgroundColor:'#027bff'
+  },
+  calculateText:{
+    fontSize: 20,
+    textAlign: 'center',
+    fontWeight: '600',
+    color:'white'
+  },
+  sum: {
+    fontSize: 20,
+    marginTop: 20,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
